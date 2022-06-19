9b50ec9c9879c00b1aaedbdde376829b7ca2fa63
leryn <pinktin@sina.com>
2022-06-19 22:35:15 +0800

    20220619: Add templates/empty projects.

--- yuque-wiki/github.py
+++ yuque-wiki/github.py
@@ -5,7 +5,7 @@ from git.repo import Repo
 def git_commit_repo(repo):
   curr_date = time.strftime("%Y%m%d", time.localtime(time.time()))
 
-  local_repo = os.path.join("repo", repo)
+  local_repo = os.path.join("./repo", repo)
   repo = Repo(local_repo)
   # repo.git.add(".")
   # repo.git.commit('-m', curr_date + r": Auto updates from yuque.")
